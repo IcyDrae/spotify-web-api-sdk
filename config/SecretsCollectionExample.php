5ad639de6e45fcd8b34024e6c9c077942626ae98
Reard Gjoni <reard.gjoni97@hotmail.co.uk>
2020-10-18 21:30:14 +0200

    added SecretsCollectionExample class and included config dir

---
+++ config/SecretsCollectionExample.php
@@ -0,0 +1,10 @@
+<?php
+
+class SecretsCollection {
+    public static string $id = "";
+    public static string $secret = "";
+    public static string $host = "";
+    public static string $baseUri = "";
+    public static string $apiUri = "";
+}
+
