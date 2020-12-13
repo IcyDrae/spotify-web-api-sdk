d19923879c3a2c796a5a32bc994c19d3752294e6
Reard Gjoni <reard.gjoni97@hotmail.co.uk>
2020-12-13 22:07:18 +0100

    feature: API rate limiting implemented- 10 request per second Changes to be committed: 	modified:   composer.json 	modified:   composer.lock 	new file:   public/api.bucket 	modified:   public/index.php

--- public/index.php
+++ public/index.php
@@ -6,12 +6,34 @@ date_default_timezone_set("Europe/Berlin");
 
 require_once dirname(__DIR__) . "/bootstrap.php";
 
+use bandwidthThrottle\tokenBucket\storage\StorageException;
 use SpotifyAPI\Http\Response;
+use bandwidthThrottle\tokenBucket\Rate;
+use bandwidthThrottle\tokenBucket\TokenBucket;
+use bandwidthThrottle\tokenBucket\storage\FileStorage;
 
 $response = new Response();
 $response->headers();
 
+/**
+ * Rate limiting- 10 request per second maximum
+ */
+try {
+    $storage = new FileStorage(__DIR__ . "/api.bucket");
+} catch (StorageException $e) {
+    printf($e->getMessage());
+}
+
+$rate = new Rate(10, Rate::SECOND);
+$bucket = new TokenBucket(10, $rate, $storage);
+$bucket->bootstrap(10);
+
+if (!$bucket->consume(1, $seconds)) {
+    http_response_code(429);
+    header(sprintf("Retry-After: %d", floor($seconds)));
+    exit();
+}
+
 require_once "router.php";
 
 ?>
-
