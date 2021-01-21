8346903010f94d787ed176fc1a5762ad762ba887
Reard Gjoni <reard.gjoni97@hotmail.co.uk>
2021-01-21 23:39:47 +0100

    documentation: phpdoc syntax

--- src/Playlists.php
+++ src/Playlists.php
@@ -122,10 +122,11 @@ class Playlists
      * Path Parameter:
      * - required
      *      - {user_id}(string): The user’s Spotify user ID.
-     * JSON Body Parameter
-     *  - required:
+     *
+     * JSON Body Parameter:
+     * - required:
      *      - name(string): The name for the new playlist, for example "Your Coolest Playlist" . This name does not need to be unique; a user may have several playlists with the same name.
-     *  - optional:
+     * - optional:
      *      - public(boolean): Defaults to true . If true the playlist will be public, if false it will be private. To be able to create private playlists, the user must have granted the playlist-modify-private scope
      *      - collaborative(boolean): Defaults to false . If true the playlist will be collaborative. Note that to create a collaborative playlist you must also set public to false . To create collaborative playlists you must have granted playlist-modify-private and playlist-modify-public scopes .
      *      - description(string): value for playlist description as displayed in Spotify Clients and in the Web API.
