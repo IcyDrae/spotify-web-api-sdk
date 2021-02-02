a74d42f76b8b9b3abc9b15e01fb485df81932ba6
Reard Gjoni <reard.gjoni97@hotmail.co.uk>
2021-02-02 22:42:19 +0100

    new Artists blueprint class

---
+++ src/Artists.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace Gjoni\SpotifyWebApiSdk;
+
+use Gjoni\SpotifyWebApiSdk\Http\Client;
+use Gjoni\SpotifyWebApiSdk\Interfaces\SdkInterface;
+use GuzzleHttp\Exception\GuzzleException;
+
+/**
+ * Responsible for communicating with the Artists API.
+ *
+ * @package Gjoni\SpotifyWebApiSdk
+ * @author Reard Gjoni <gjoni-r@hotmail.com>
+ * @link https://developer.spotify.com/documentation/web-api/reference/#category-artists
+ */
+class Artists
+{
+    /**
+     * @var Client $client Custom client object
+     */
+    private Client $client;
+
+    /**
+     * Playlists constructor.
+     *
+     * Initializes the client object, response, headers and client parameters.
+     *
+     * @param SdkInterface $sdk
+     */
+    public function __construct(SdkInterface $sdk)
+    {
+        $this->client = new Client($sdk, [
+            "base_uri" => SdkConstants::API_URL,
+            "timeout" => 1,
+            "allow_redirects" => ["track_redirects" => true]
+        ]);
+    }
+
+}
