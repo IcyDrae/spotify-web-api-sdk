8f5958e8c8d01eb4441a9924819d4253f6dc228a
ReardGjoni <reard.gjoni97@hotmail.co.uk>
2021-03-14 17:31:23 +0100

    feature(Library): added removeAlbums & checkSavedAlbums methods Changes to be committed: 	modified:   src/Library.php

--- src/Library.php
+++ src/Library.php
@@ -76,11 +76,11 @@ class Library
      *
      * Query parameter:
      * - required
-     *      - ids: A comma-separated list of the Spotify IDs. For example: ids=4iV5W9uYEdYUVa79Axb7Rh,1301WleyT98MSxVHPZCA6M. Maximum: 50 IDs.
+     *      - ids(string): A comma-separated list of the Spotify IDs. For example: ids=4iV5W9uYEdYUVa79Axb7Rh,1301WleyT98MSxVHPZCA6M. Maximum: 50 IDs.
      *
      * JSON body parameter:
      * - optional
-     *      - ids: array[string]: A JSON array of the Spotify IDs. For example: ["4iV5W9uYEdYUVa79Axb7Rh", "1301WleyT98MSxVHPZCA6M"] A maximum of 50 items can be specified in one request. Note: if the ids parameter is present in the query string, any IDs listed here in the body will be ignored.
+     *      - ids(array[string]): A JSON array of the Spotify IDs. For example: ["4iV5W9uYEdYUVa79Axb7Rh", "1301WleyT98MSxVHPZCA6M"] A maximum of 50 items can be specified in one request. Note: if the ids parameter is present in the query string, any IDs listed here in the body will be ignored.
      *
      * Response:
      *
@@ -95,6 +95,63 @@ class Library
     public function saveAlbums(array $options = []): string
     {
         return $this->client->delegate("PUT", SdkConstants::LIBRARY["ALBUMS"], $options);
+    }
+
+    /**
+     * Remove one or more albums from the current user’s ‘Your Music’ library.
+     *
+     * Header:
+     * - required
+     *      - Authorization(string): A valid access token from the Spotify Accounts service: see the Web API Authorization Guide for details. Modification of the current user’s “Your Music” collection requires authorization of the user-library-modify scope.
+     * - optional
+     *      - Content-Type(string): Required if the IDs are passed in the request body, otherwise ignored. The content type of the request body: application/json
+     *
+     * Query parameter:
+     * - required
+     *      - ids(string): A comma-separated list of the Spotify IDs. For example: ids=4iV5W9uYEdYUVa79Axb7Rh,1301WleyT98MSxVHPZCA6M. Maximum: 50 IDs.
+     *
+     * JSON body parameter:
+     * - optional
+     *      - ids(array[string]): A JSON array of the Spotify IDs. For example: ["4iV5W9uYEdYUVa79Axb7Rh", "1301WleyT98MSxVHPZCA6M"] A maximum of 50 items can be specified in one request. Note: if the ids parameter is present in the query string, any IDs listed here in the body will be ignored.
+     *
+     * Response:
+     *
+     * On success, the HTTP status code in the response header is 200 Success.
+     * On error, the header status code is an error code and the response body contains an error object.
+     * Trying to remove an album when you do not have the user’s authorization returns error 403 Forbidden.
+     *
+     * @param array $options (optional) Request parameters
+     * @throws GuzzleException
+     * @return string
+     */
+    public function removeAlbums(array $options = []): string
+    {
+        return $this->client->delegate("DELETE", SdkConstants::LIBRARY["ALBUMS"], $options);
+    }
+
+    /**
+     * Check if one or more albums is already saved in the current Spotify user’s ‘Your Music’ library.
+     *
+     * Header:
+     * - required
+     *      - A valid access token from the Spotify Accounts service: see the Web API Authorization Guide for details. The user-library-read scope must have been authorized by the user.
+     *
+     * Query parameter:
+     * - required
+     *      - ids: A comma-separated list of the Spotify IDs for the albums. Maximum: 50 IDs.
+     *
+     * Response:
+     *
+     * On success, the HTTP status code in the response header is 200 OK and the response body contains a JSON array of true or false values, in the same order in which the ids were specified.
+     * On error, the header status code is an error code and the response body contains an error object.
+     *
+     * @param array $options (optional) Request parameters
+     * @throws GuzzleException
+     * @return string
+     */
+    public function checkSavedAlbums(array $options = []): string
+    {
+        return $this->client->delegate("GET", SdkConstants::LIBRARY["ALBUMS"] . "/contains", $options);
     }
 
 }
