dd5c40bde43f1a7f865fec99f238fd13c0284617
ReardGjoni <reard.gjoni97@hotmail.co.uk>
2021-03-14 17:38:23 +0100

    feature(Library): added getSavedTracks method Changes to be committed: 	modified:   src/Library.php

--- src/Library.php
+++ src/Library.php
@@ -134,11 +134,11 @@ class Library
      *
      * Header:
      * - required
-     *      - A valid access token from the Spotify Accounts service: see the Web API Authorization Guide for details. The user-library-read scope must have been authorized by the user.
+     *      - Authorization(string): A valid access token from the Spotify Accounts service: see the Web API Authorization Guide for details. The user-library-read scope must have been authorized by the user.
      *
      * Query parameter:
      * - required
-     *      - ids: A comma-separated list of the Spotify IDs for the albums. Maximum: 50 IDs.
+     *      - ids(string): A comma-separated list of the Spotify IDs for the albums. Maximum: 50 IDs.
      *
      * Response:
      *
@@ -152,6 +152,33 @@ class Library
     public function checkSavedAlbums(array $options = []): string
     {
         return $this->client->delegate("GET", SdkConstants::LIBRARY["ALBUMS"] . "/contains", $options);
+    }
+
+    /**
+     * Get a list of the songs saved in the current Spotify user’s ‘Your Music’ library.
+     *
+     * Header:
+     * - required
+     *      - Authorization(string): A valid access token from the Spotify Accounts service: see the Web API Authorization Guide for details. The user-library-read scope must have been authorized by the user.
+     *
+     * Query parameter:
+     * - optional
+     *      - market(string): An ISO 3166-1 alpha-2 country code or the string from_token. Provide this parameter if you want to apply Track Relinking.
+     *      - limit(integer): The maximum number of objects to return. Default: 20. Minimum: 1. Maximum: 50.
+     *      - offset(integer): The index of the first object to return. Default: 0 (i.e., the first object). Use with limit to get the next set of objects.
+     *
+     * Response:
+     *
+     * On success, the HTTP status code in the response header is 200 OK and the response body contains an array of saved track objects (wrapped in a paging object) in JSON format.
+     * On error, the header status code is an error code and the response body contains an error object.
+     *
+     * @param array $options (optional) Request parameters
+     * @throws GuzzleException
+     * @return string
+     */
+    public function getSavedTracks(array $options = []): string
+    {
+        return $this->client->delegate("GET", SdkConstants::LIBRARY["TRACKS"], $options);
     }
 
 }
