b98935262ecf1f11ed47d51b9417f5caed4a0792
ReardGjoni <reard.gjoni97@hotmail.co.uk>
2021-03-20 19:19:30 +0100

    feature(Shows): added new method getEpisodes Changes to be committed: 	modified:   src/Shows.php

--- src/Shows.php
+++ src/Shows.php
@@ -97,4 +97,38 @@ class Shows
         return $this->client->delegate("GET", SdkConstants::SHOWS . "/$id", $options);
     }
 
+    /**
+     * Get Spotify catalog information about an show’s episodes. Optional parameters can be used to limit the number of episodes returned.
+     *
+     * Header:
+     * - required
+     *      - Authorization(string): Get Spotify catalog information for a single show identified by its unique Spotify ID.
+     *
+     * Path parameter:
+     * - required
+     *      - {id}(string): The Spotify ID for the show.
+     *
+     * Query parameter:
+     * - optional
+     *      - market(string): An ISO 3166-1 alpha-2 country code. If a country code is specified, only shows and episodes that are available in that market will be returned. If a valid user access token is specified in the request header, the country associated with the user account will take priority over this parameter. Note: If neither market or user country are provided, the content is considered unavailable for the client. Users can view the country that is associated with their account in the account settings.
+     *      - limit(integer): The maximum number of episodes to return. Default: 20. Minimum: 1. Maximum: 50.
+     *      - offset(integer): The index of the first episode to return. Default: 0 (the first object). Use with limit to get the next set of episodes.
+     *
+     * Response:
+     *
+     * On success, the HTTP status code in the response header is 200 OK and the response body contains an array of simplified episode objects (wrapped in a paging object) in JSON format.
+     * On error, the header status code is an error code and the response body contains an error object.
+     * If a show is unavailable in the given market the HTTP status code in the response header is 404 NOT FOUND.
+     * Unavailable episodes are filtered out.
+     *
+     * @param string $id The show id
+     * @param array $options (optional) Request parameters
+     * @throws GuzzleException
+     * @return string
+     */
+    public function getEpisodes(string $id, array $options = []): string
+    {
+        return $this->client->delegate("GET", SdkConstants::SHOWS . "/$id/episodes", $options);
+    }
+
 }
