1be65055982e89743a9f6a76df789eb2b34e377a
ReardGjoni <reard.gjoni97@hotmail.co.uk>
2021-04-04 18:20:39 +0200

    feature(Testing): new namespace for tests, first empty tests, gitignote updated accordingly Changes to be committed: 	modified:   .gitignore 	modified:   composer.json 	new file:   tests/Unit/AuthorizationTest.php

---
+++ tests/Unit/AuthorizationTest.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Gjoni\SpotifyWebApiSdk\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class AuthorizationTest extends TestCase
+{
+
+    public function test_1()
+    {
+        $this->assertSame(1, 1);
+        $this->assertSame(0, 0);
+        $this->assertSame(1, 1);
+    }
+
+    public function test_2()
+    {
+        $this->assertSame(1, 1);
+        $this->assertSame(0, 0);
+        $this->assertSame(1, 1);
+    }
+
+    public function test_3()
+    {
+        return true;
+    }
+}
