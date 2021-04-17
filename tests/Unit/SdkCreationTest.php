13f8ecbbcccfd7c4c06843251f24da53c2972169
ReardGjoni <reard.gjoni97@hotmail.co.uk>
2021-04-17 16:50:58 +0200

    tests(SdkCreationTest): covered the scopes format being what it should Changes to be committed: 	modified:   tests/Unit/SdkCreationTest.php

--- tests/Unit/SdkCreationTest.php
+++ tests/Unit/SdkCreationTest.php
@@ -13,12 +13,21 @@ class SdkCreationTest extends TestCase
     {
         $sdk = new Sdk("ZYDPLLBWSK3MVQJSIYHB1OR2JXCY0X2C5UJ2QAR2MAAIT5Q", "YHB1OR2JXCY0X2C5UJ2QAR2MAAIT5Q", [
             "user-read-private",
+            "user-follow-modify",
+            "some-other-scope"
         ]);
 
         $this->assertIsObject($sdk);
         $this->assertIsString($sdk->getClientId());
         $this->assertIsString($sdk->getClientSecret());
         $this->assertIsString($sdk->getScopes());
+
+        # Test the format of the scopes as described in the SdkInterface
+        $this->assertEquals(
+            "user-read-private user-follow-modify some-other-scope",
+            $sdk->getScopes(),
+            "Scopes format is not a space separated string!"
+        );
     }
 
     public function testSetAccessToken()
