c6442d6b752d9ecf1a9a091128470f76f2a3b2dc
ReardGjoni <reard.gjoni97@hotmail.co.uk>
2021-05-16 20:10:12 +0200

    tests: added tests for Albums, Artists & Browse API's Changes to be committed: 	new file:   tests/Unit/AlbumsTest.php 	new file:   tests/Unit/ArtistsTest.php 	new file:   tests/Unit/BrowseTest.php 	new file:   tests/Unit/fixtures/albums-get-multiple.json 	new file:   tests/Unit/fixtures/albums-get-single.json 	new file:   tests/Unit/fixtures/albums-get-tracks.json 	new file:   tests/Unit/fixtures/artists-get-albums.json 	new file:   tests/Unit/fixtures/artists-get-multiple.json 	new file:   tests/Unit/fixtures/artists-get-related.json 	new file:   tests/Unit/fixtures/artists-get-single.json 	new file:   tests/Unit/fixtures/artists-get-top-tracks.json 	new file:   tests/Unit/fixtures/browse-get-categories.json 	new file:   tests/Unit/fixtures/browse-get-category-playlists.json 	new file:   tests/Unit/fixtures/browse-get-category.json 	new file:   tests/Unit/fixtures/browse-get-featured-playlists.json 	new file:   tests/Unit/fixtures/browse-get-new-releases.json 	new file:   tests/Unit/fixtures/browse-get-recommendation-genres.json 	new file:   tests/Unit/fixtures/browse-get-recommendations.json

---
+++ tests/Unit/ArtistsTest.php
@@ -0,0 +1,178 @@
+<?php
+
+
+namespace Gjoni\SpotifyWebApiSdk\Tests\Unit;
+
+use function getFixture;
+use Gjoni\SpotifyWebApiSdk\Artists;
+use Gjoni\SpotifyWebApiSdk\Interfaces\SdkInterface;
+use Gjoni\SpotifyWebApiSdk\Sdk;
+use PHPUnit\Framework\TestCase;
+
+class ArtistsTest extends TestCase
+{
+    private SdkInterface $sdk;
+    private string $clientId = "ZYDPLLBWSK3MVQJSIYHB1OR2JXCY0X2C5UJ2QAR2MAAIT5Q";
+    private string $clientSecret = "54zYUAzCFg22ek7iXjATrb0L5gA7Pjry";
+    private array $scopes = [
+        "user-read-private",
+        "user-follow-modify",
+    ];
+    private string $redirectUri = "https://example.com";
+
+    protected function setUp(): void
+    {
+        $this->sdk = new Sdk(
+            $this->clientId,
+            $this->clientSecret,
+            $this->scopes,
+            $this->redirectUri
+        );
+    }
+
+    private function setUpMockObject(string $methodName, string $returnValue)
+    {
+        $mock = $this
+            ->getMockBuilder(Artists::class)
+            ->setConstructorArgs([$this->sdk])
+            ->onlyMethods([$methodName])
+            ->getMock();
+
+        $mock
+            ->expects($this->once())
+            ->method($methodName)
+            ->withAnyParameters()
+            ->willReturn($returnValue);
+
+        return $mock;
+    }
+
+    public function testCreateAlbumsInstance()
+    {
+        $client = new Artists($this->sdk);
+
+        $this->assertIsObject($client);
+    }
+
+    public function testGetMultiple()
+    {
+        $response = getFixture("artists-get-multiple");
+        $returnValue = $response;
+        $returnValueDecoded = json_decode($returnValue, true);
+
+        $artists = $this->setUpMockObject(
+            "getMultiple",
+            $returnValue);
+
+        $this->assertEquals(
+            $returnValue,
+            $artists->getMultiple([
+                "query_params" => [
+                    "ids" => "0oSGxfWSnnOXhD2fKuz2Gy,3dBVyJ7JuOMt4GE9607Qin"
+                ]
+            ])
+        );
+
+        self::assertJson($returnValue);
+
+        $this->assertIsArray($returnValueDecoded);
+        $this->assertIsArray($returnValueDecoded["artists"]);
+    }
+
+    public function testGetSingle()
+    {
+        $response = getFixture("artists-get-single");
+        $returnValue = $response;
+        $returnValueDecoded = json_decode($returnValue, true);
+
+        $artists = $this->setUpMockObject(
+            "getSingle",
+            $returnValue);
+
+        $this->assertEquals(
+            $returnValue,
+            $artists->getSingle("0OdUWJ0sBjDrqHygGUXeCF", [])
+        );
+
+        self::assertJson($returnValue);
+
+        $this->assertIsArray($returnValueDecoded);
+        $this->assertIsArray($returnValueDecoded["external_urls"]);
+        $this->assertIsArray($returnValueDecoded["followers"]);
+    }
+
+    public function testGetTopTracks()
+    {
+        $response = getFixture("artists-get-top-tracks");
+        $returnValue = $response;
+        $returnValueDecoded = json_decode($returnValue, true);
+
+        $artists = $this->setUpMockObject(
+            "getTopTracks",
+            $returnValue);
+
+        $this->assertEquals(
+            $returnValue,
+            $artists->getTopTracks("0oSGxfWSnnOXhD2fKuz2Gy", [
+                "query_params" => [
+                    "market" => "US"
+                ]
+            ])
+        );
+
+        self::assertJson($returnValue);
+
+        $this->assertIsArray($returnValueDecoded);
+        $this->assertIsArray($returnValueDecoded["tracks"]);
+        $this->assertIsArray($returnValueDecoded["tracks"][0]["album"]);
+    }
+
+    public function testGetRelatedArtists()
+    {
+        $response = getFixture("artists-get-related");
+        $returnValue = $response;
+        $returnValueDecoded = json_decode($returnValue, true);
+
+        $artists = $this->setUpMockObject(
+            "getRelatedArtists",
+            $returnValue);
+
+        $this->assertEquals(
+            $returnValue,
+            $artists->getRelatedArtists("0oSGxfWSnnOXhD2fKuz2Gy", [])
+        );
+
+        self::assertJson($returnValue);
+
+        $this->assertIsArray($returnValueDecoded);
+        $this->assertIsArray($returnValueDecoded["artists"]);
+        $this->assertIsArray($returnValueDecoded["artists"][0]["external_urls"]);
+    }
+
+    public function testGetAlbums()
+    {
+        $response = getFixture("artists-get-albums");
+        $returnValue = $response;
+        $returnValueDecoded = json_decode($returnValue, true);
+
+        $artists = $this->setUpMockObject(
+            "getAlbums",
+            $returnValue);
+
+        $this->assertEquals(
+            $returnValue,
+            $artists->getAlbums("0oSGxfWSnnOXhD2fKuz2Gy", [
+                "query_params" => [
+                    "include_groups" => "album,single,appears_on,compilation",
+                    "limit" => 1
+                ]
+            ])
+        );
+
+        self::assertJson($returnValue);
+
+        $this->assertIsArray($returnValueDecoded);
+        $this->assertIsString($returnValueDecoded["href"]);
+        $this->assertIsArray($returnValueDecoded["items"]);
+    }
+}
---
+++ tests/Unit/BrowseTest.php
@@ -0,0 +1,212 @@
+<?php
+
+
+namespace Gjoni\SpotifyWebApiSdk\Tests\Unit;
+
+use function getFixture;
+use Gjoni\SpotifyWebApiSdk\Browse;
+use Gjoni\SpotifyWebApiSdk\Interfaces\SdkInterface;
+use Gjoni\SpotifyWebApiSdk\Sdk;
+use PHPUnit\Framework\TestCase;
+
+class   BrowseTest extends TestCase
+{
+    private SdkInterface $sdk;
+    private string $clientId = "ZYDPLLBWSK3MVQJSIYHB1OR2JXCY0X2C5UJ2QAR2MAAIT5Q";
+    private string $clientSecret = "54zYUAzCFg22ek7iXjATrb0L5gA7Pjry";
+    private array $scopes = [
+        "user-read-private",
+        "user-follow-modify",
+    ];
+    private string $redirectUri = "https://example.com";
+
+    protected function setUp(): void
+    {
+        $this->sdk = new Sdk(
+            $this->clientId,
+            $this->clientSecret,
+            $this->scopes,
+            $this->redirectUri
+        );
+    }
+
+    private function setUpMockObject(string $methodName, string $returnValue)
+    {
+        $mock = $this
+            ->getMockBuilder(Browse::class)
+            ->setConstructorArgs([$this->sdk])
+            ->onlyMethods([$methodName])
+            ->getMock();
+
+        $mock
+            ->expects($this->once())
+            ->method($methodName)
+            ->withAnyParameters()
+            ->willReturn($returnValue);
+
+        return $mock;
+    }
+
+    public function testCreateBrowseInstance()
+    {
+        $client = new Browse($this->sdk);
+
+        $this->assertIsObject($client);
+    }
+
+    public function testGetNewReleases()
+    {
+        $response = getFixture("browse-get-new-releases");
+        $returnValue = $response;
+        $returnValueDecoded = json_decode($returnValue, true);
+
+        $browse = $this->setUpMockObject(
+            "getNewReleases",
+            $returnValue);
+
+        $this->assertEquals(
+            $returnValue,
+            $browse->getNewReleases([])
+        );
+
+        self::assertJson($returnValue);
+
+        $this->assertIsArray($returnValueDecoded);
+        $this->assertIsArray($returnValueDecoded["albums"]);
+    }
+
+    public function testGetFeaturedPlaylists()
+    {
+        $response = getFixture("browse-get-featured-playlists");
+        $returnValue = $response;
+        $returnValueDecoded = json_decode($returnValue, true);
+
+        $browse = $this->setUpMockObject(
+            "getFeaturedPlaylists",
+            $returnValue);
+
+        $this->assertEquals(
+            $returnValue,
+            $browse->getFeaturedPlaylists([])
+        );
+
+        self::assertJson($returnValue);
+
+        $this->assertIsArray($returnValueDecoded);
+        $this->assertIsString($returnValueDecoded["message"]);
+        $this->assertIsArray($returnValueDecoded["playlists"]);
+    }
+
+    public function testGetCategories()
+    {
+        $response = getFixture("browse-get-categories");
+        $returnValue = $response;
+        $returnValueDecoded = json_decode($returnValue, true);
+
+        $browse = $this->setUpMockObject(
+            "getCategories",
+            $returnValue);
+
+        $this->assertEquals(
+            $returnValue,
+            $browse->getCategories([])
+        );
+
+        self::assertJson($returnValue);
+
+        $this->assertIsArray($returnValueDecoded);
+        $this->assertIsArray($returnValueDecoded["categories"]);
+    }
+
+    public function testGetCategory()
+    {
+        $response = getFixture("browse-get-category");
+        $returnValue = $response;
+        $returnValueDecoded = json_decode($returnValue, true);
+
+        $browse = $this->setUpMockObject(
+            "getCategory",
+            $returnValue);
+
+        $this->assertEquals(
+            $returnValue,
+            $browse->getCategory("dinner", [])
+        );
+
+        self::assertJson($returnValue);
+
+        $this->assertIsArray($returnValueDecoded);
+        $this->assertIsString($returnValueDecoded["id"]);
+    }
+
+    public function testGetCategoryPlaylists()
+    {
+        $response = getFixture("browse-get-category-playlists");
+        $returnValue = $response;
+        $returnValueDecoded = json_decode($returnValue, true);
+
+        $browse = $this->setUpMockObject(
+            "getCategoryPlaylists",
+            $returnValue);
+
+        $this->assertEquals(
+            $returnValue,
+            $browse->getCategoryPlaylists("dinner", [])
+        );
+
+        self::assertJson($returnValue);
+
+        $this->assertIsArray($returnValueDecoded);
+        $this->assertIsArray($returnValueDecoded["playlists"]);
+    }
+
+    public function testGetRecommendations()
+    {
+        $response = getFixture("browse-get-recommendations");
+        $returnValue = $response;
+        $returnValueDecoded = json_decode($returnValue, true);
+
+        $browse = $this->setUpMockObject(
+            "getRecommendations",
+            $returnValue);
+
+        $this->assertEquals(
+            $returnValue,
+            $browse->getRecommendations([
+                "query_params" => [
+                    "market" => "DE",
+                    "seed_artists" => "6rqhFgbbKwnb9MLmUQDhG6",
+                    "seed_genres" => "pop,hip-hop",
+                    "seed_tracks" => "0c6xIDDpzE81m2q797ordA"
+                ]
+            ])
+        );
+
+        self::assertJson($returnValue);
+
+        $this->assertIsArray($returnValueDecoded);
+        $this->assertIsArray($returnValueDecoded["tracks"]);
+        $this->assertIsArray($returnValueDecoded["seeds"]);
+    }
+
+    public function testGetRecommendationGenres()
+    {
+        $response = getFixture("browse-get-recommendation-genres");
+        $returnValue = $response;
+        $returnValueDecoded = json_decode($returnValue, true);
+
+        $browse = $this->setUpMockObject(
+            "getRecommendationGenres",
+            $returnValue);
+
+        $this->assertEquals(
+            $returnValue,
+            $browse->getRecommendationGenres([])
+        );
+
+        self::assertJson($returnValue);
+
+        $this->assertIsArray($returnValueDecoded);
+        $this->assertIsArray($returnValueDecoded["genres"]);
+    }
+}
