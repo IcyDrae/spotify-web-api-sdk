6ecae6310876e22012b8ab6eba5ab9fb82183bde
ReardGjoni <reard.gjoni97@hotmail.co.uk>
2021-05-19 19:37:15 +0200

    tests: added tests for the Markets API Changes to be committed: 	new file:   tests/Unit/MarketsTest.php 	new file:   tests/Unit/fixtures/markets-get-available.json

---
+++ tests/Unit/MarketsTest.php
@@ -0,0 +1,78 @@
+<?php
+
+
+namespace Gjoni\SpotifyWebApiSdk\Tests\Unit;
+
+use function getFixture;
+use Gjoni\SpotifyWebApiSdk\Markets;
+use Gjoni\SpotifyWebApiSdk\Interfaces\SdkInterface;
+use Gjoni\SpotifyWebApiSdk\Sdk;
+use PHPUnit\Framework\TestCase;
+
+class MarketsTest extends TestCase
+{
+    private SdkInterface $sdk;
+    private string $clientId = "ZYDPLLBWSK3MVQJSIYHB1OR2JXCY0X2C5UJ2QAR2MAAIT5Q";
+    private string $clientSecret = "54zYUAzCFg22ek7iXjATrb0L5gA7Pjry";
+    private array $scopes = [
+        "user-read-private",
+        "user-follow-modify",
+    ];
+    private string $redirectUri = "https://example.com";
+
+    protected function setUp(): void
+    {
+        $this->sdk = new Sdk(
+            $this->clientId,
+            $this->clientSecret,
+            $this->scopes,
+            $this->redirectUri
+        );
+    }
+
+    private function setUpMockObject(string $methodName, string $returnValue)
+    {
+        $mock = $this
+            ->getMockBuilder(Markets::class)
+            ->setConstructorArgs([$this->sdk])
+            ->onlyMethods([$methodName])
+            ->getMock();
+
+        $mock
+            ->expects($this->once())
+            ->method($methodName)
+            ->withAnyParameters()
+            ->willReturn($returnValue);
+
+        return $mock;
+    }
+
+    public function testCreateMarketsInstance()
+    {
+        $markets = new Markets($this->sdk);
+
+        $this->assertIsObject($markets);
+    }
+
+    public function testGetAvailable()
+    {
+        $response = getFixture("markets-get-available");
+        $returnValue = $response;
+        $returnValueDecoded = json_decode($returnValue, true);
+
+        $albums = $this->setUpMockObject(
+            "getAvailable",
+            $returnValue);
+
+        $this->assertEquals(
+            $returnValue,
+            $albums->getAvailable([])
+        );
+
+        self::assertJson($returnValue);
+
+        $this->assertIsArray($returnValueDecoded);
+        $this->assertIsArray($returnValueDecoded["markets"]);
+    }
+
+}
