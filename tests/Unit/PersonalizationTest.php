68f9e14621b70cd78797f27b17b41b03f27efb76
ReardGjoni <reard.gjoni97@hotmail.co.uk>
2021-05-19 19:53:43 +0200

    tests: added tests for the Personalization API Changes to be committed: 	new file:   tests/Unit/PersonalizationTest.php 	new file:   tests/Unit/fixtures/personalization-get-top-artists.json 	new file:   tests/Unit/fixtures/personalization-get-top-tracks.json

---
+++ tests/Unit/PersonalizationTest.php
@@ -0,0 +1,98 @@
+<?php
+
+
+namespace Gjoni\SpotifyWebApiSdk\Tests\Unit;
+
+use function getFixture;
+use Gjoni\SpotifyWebApiSdk\Personalization;
+use Gjoni\SpotifyWebApiSdk\Interfaces\SdkInterface;
+use Gjoni\SpotifyWebApiSdk\Sdk;
+use PHPUnit\Framework\TestCase;
+
+class PersonalizationTest extends TestCase
+{
+    private SdkInterface $sdk;
+    private string $clientId = "ZYDPLLBWSK3MVQJSIYHB1OR2JXCY0X2C5UJ2QAR2MAAIT5Q";
+    private string $clientSecret = "54zYUAzCFg22ek7iXjATrb0L5gA7Pjry";
+    private array $scopes = [
+        "user-read-private",
+        "user-follow-modify",
+    ];
+    private string $redirectUri = "https://example.com";
+
+    protected function setUp(): void
+    {
+        $this->sdk = new Sdk(
+            $this->clientId,
+            $this->clientSecret,
+            $this->scopes,
+            $this->redirectUri
+        );
+    }
+
+    private function setUpMockObject(string $methodName, string $returnValue)
+    {
+        $mock = $this
+            ->getMockBuilder(Personalization::class)
+            ->setConstructorArgs([$this->sdk])
+            ->onlyMethods([$methodName])
+            ->getMock();
+
+        $mock
+            ->expects($this->once())
+            ->method($methodName)
+            ->withAnyParameters()
+            ->willReturn($returnValue);
+
+        return $mock;
+    }
+
+    public function testCreatePersonalizationInstance()
+    {
+        $personalization = new Personalization($this->sdk);
+
+        $this->assertIsObject($personalization);
+    }
+
+    public function testGetTopTracks()
+    {
+        $response = getFixture("personalization-get-top-tracks");
+        $returnValue = $response;
+        $returnValueDecoded = json_decode($returnValue, true);
+
+        $albums = $this->setUpMockObject(
+            "getTop",
+            $returnValue);
+
+        $this->assertEquals(
+            $returnValue,
+            $albums->getTop("tracks", [])
+        );
+
+        self::assertJson($returnValue);
+
+        $this->assertIsArray($returnValueDecoded);
+        $this->assertIsArray($returnValueDecoded["items"]);
+    }
+
+    public function testGetTopArtists()
+    {
+        $response = getFixture("personalization-get-top-artists");
+        $returnValue = $response;
+        $returnValueDecoded = json_decode($returnValue, true);
+
+        $albums = $this->setUpMockObject(
+            "getTop",
+            $returnValue);
+
+        $this->assertEquals(
+            $returnValue,
+            $albums->getTop("tracks", [])
+        );
+
+        self::assertJson($returnValue);
+
+        $this->assertIsArray($returnValueDecoded);
+        $this->assertIsArray($returnValueDecoded["items"]);
+    }
+}
