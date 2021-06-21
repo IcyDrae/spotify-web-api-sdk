1c9036a5607626e73027fa10494554aa7ec68733
MatrixEternal <reard.gjoni97@hotmail.co.uk>
2021-06-21 21:42:09 +0200

    fix: bug where function 'getFixture' would be declared multiple times Changes to be committed: 	modified:   tests/functions.php

--- tests/functions.php
+++ tests/functions.php
@@ -1,13 +1,15 @@
 <?php
 
 
-function getFixture(string $name): string {
-    $fileContents = "";
-    $path = getcwd() . "/tests/Unit/fixtures/$name.json";
+if (function_exists('getFixture')) {
+    function getFixture(string $name): string {
+        $fileContents = "";
+        $path = getcwd() . "/tests/Unit/fixtures/$name.json";
 
-    if (file_exists($path)) {
-        $fileContents = file_get_contents($path);
+        if (file_exists($path)) {
+            $fileContents = file_get_contents($path);
+        }
+
+        return $fileContents;
     }
-
-    return $fileContents;
 }
